c01224525aeeec1b513bf8700bef72195efdf469
Wenwei Huang <dut.hww@gmail.com>
2019-06-06 16:34:11 +0800

    Remove submodules

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
 #sudo apt install vim-nox-py2 # python2 support for vim
-git submodule update --init --recursive
+
 cp -r .vim ~/.vim
+git clone https://github.com/kien/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
+git clone https://github.com/davidhalter/jedi-vim ~/.vim/bundle/jedi-vim
+git clone https://github.com/Lokaltog/vim-powerline.git ~/.vim/bundle/vim-powerline
+git clone https://github.com/pedrohdz/vim-yaml-folds ~/.vim/bundle/vim-yaml-folds
+git clone https://github.com/scrooloose/nerdtree ~/.vim/bundle/nerdtree
+
 cp -r .vimrc ~/
 cp -r .gitconfig ~/
